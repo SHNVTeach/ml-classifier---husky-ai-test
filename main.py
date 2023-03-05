80037c62e86e703f83a74ff0a9ffe8d0126e2bac
SHNVTeach <117742335+SHNVTeach@users.noreply.github.com>
2023-03-05 21:23:15 +0800

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+def my_function():
+    huskylens.save_model_to_tf_card(HUSKYLENSMode.SAVE, 0)
+buttonClicks.on_button_double_clicked(buttonClicks.AorB.A, my_function)
+
 def on_button_pressed_a():
     global id2
     id2 += 1
+    pins.digital_write_pin(DigitalPin.P0, 1)
     basic.show_number(id2)
+    if id2 > 6:
+        id2 = 0
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_ab():
@@ -9,6 +16,7 @@ def on_button_pressed_ab():
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 id2 = 0
+index = 0
 huskylens.init_i2c()
 huskylens.init_mode(protocolAlgorithm.OBJECTCLASSIFICATION)
 basic.show_leds("""
@@ -26,8 +34,17 @@ huskylens.write_name(4, "Face")
 huskylens.write_name(5, "Pants")
 huskylens.write_name(6, "Pattern Shirt")
 id2 = 0
+basic.show_number(id2)
 
 def on_forever():
     if input.button_is_pressed(Button.B):
         huskylens.write_learn1(id2)
 basic.forever(on_forever)
+
+def on_forever2():
+    huskylens.request()
+    if huskylens.is_appear(1, HUSKYLENSResultType_t.HUSKYLENS_RESULT_BLOCK):
+        music.play_tone(262, music.beat(BeatFraction.HALF))
+    else:
+        music.play_tone(330, music.beat(BeatFraction.HALF))
+basic.forever(on_forever2)
--- main.ts
+++ main.ts
@@ -1,7 +1,10 @@
+buttonClicks.onButtonDoubleClicked(buttonClicks.AorB.A, function () {
+    huskylens.saveModelToTFCard(HUSKYLENSMode.SAVE, 0)
+})
 input.onButtonPressed(Button.A, function () {
     id += 1
     basic.showNumber(id)
-    if (id > 6) {
+    if (id > 5) {
         id = 0
     }
 })
@@ -18,18 +21,22 @@ basic.showLeds(`
     . . . . .
     . . . . .
     `)
-huskylens.writeName(0, "Blue")
-huskylens.writeName(1, "Plain Shirt")
-huskylens.writeName(2, "Nothing")
+huskylens.writeName(0, "Background")
+huskylens.writeName(1, "T-Shirt")
+huskylens.writeName(2, "Pants")
 huskylens.writeName(3, "Dress")
 huskylens.writeName(4, "Face")
-huskylens.writeName(5, "Pants")
-huskylens.writeName(6, "Pattern Shirt")
+huskylens.writeName(5, "Pattern Shirt")
 id = 0
-let index = 0
 basic.showNumber(id)
 basic.forever(function () {
     if (input.buttonIsPressed(Button.B)) {
         huskylens.writeLearn1(id)
+    }
+})
+basic.forever(function () {
+    huskylens.request()
+    if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        basic.showString("1")
     }
 })
